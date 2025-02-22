6c6b9379fd7af1e44e712f1cb35f72304a732308
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-02-22 22:19:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const makeChange = (c) => {
   // your name here
+	  return {
+        q: Math.floor(amount / 25),
+        d: Math.floor((amount % 25) / 10),
+        n: Math.floor((amount % 25 % 10) / 5),
+        p: amount % 5
+    };
 };
 
 // Do not the change the code below
